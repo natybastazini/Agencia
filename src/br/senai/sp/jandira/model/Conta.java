f6a1a4196e40702d7f8bd1d73701003136b284c5
natybastazini <natybastazini7@gmail.com>
2023-09-27 17:21:27 -0300

    precisa finalizar!!

--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -1,14 +1,47 @@
 package br.senai.sp.jandira.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
 
     boolean validaSaldo;
 
-    private int numeroConta = 1234;
+    private int numeroConta;
 
-    private double saldo = 9652;
+    private double saldo = 0;
 
     private String agencia = "000-35-XX";
+
+    private Cliente cliente;
+
+    List<Conta> listConta = new ArrayList<>();
+
+    public void adicionarContaList(Conta conta){
+        listConta.add(conta);
+    }
+
+    public Conta pesquisarConta(long cpf){
+        for (Conta conta: listConta){
+            long validaCpf = conta.cliente.getCpf();
+            if (validaCpf == cpf){
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void gerarConta(Cliente cliente){
+        numeroConta = (int) (Math.random()*10000);
+        this.cliente = cliente;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
 
     public void realizarDeposito(Double valor){
         this.saldo += valor;
@@ -36,12 +69,22 @@ public class Conta {
         System.out.println("Seu saldo é: " + this.saldo);
     }
 
-    public void avaliaSaldo(double valor){
+    public void avaliaSaque(double valor){
         if(valor > this.saldo){
             validaSaldo = false;
         }else{
             validaSaldo = true;
         }
+    }
+
+    public void realizarTransferencia(double valor){
+
+        if(valor > saldo){
+            System.out.println("Você não tem saldo para essa transferência");
+        } else {
+            saldo - valor
+        }
+
     }
 
 
